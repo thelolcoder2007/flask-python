c99b0c19bd3b9cedeb9ab7ed50feb5be9707147e
thelolcoder2007 <thomas.erents@gmail.com>
2021-07-07 14:38:40 +0200

    add email support

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,7 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +13,18 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+if not app.debug:
+    auth = None
+    if app.config['MAIL_SERVER']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    secure = None
+    if app.config['MAIL_USE_TLS']:
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr = 'no-reply@'+app.config['MAIL_SERVER'],
+        toaddrs=app.config["ADMINS"], subject='flask failure!',
+        cridentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 from app import routes, models, errors
--- flask/config.py
+++ flask/config.py
@@ -3,3 +3,9 @@ basedir = os.path.abspath(os.path.dirname('fleur home huiswerk assistent.py'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fleur_home_huiswerk_assistent.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
