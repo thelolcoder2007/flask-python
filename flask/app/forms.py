65cd99630e51e07fd7ad378f412ea6e96d540c63
thelolcoder2007 <thomas.erents@gmail.com>
2021-07-07 13:59:54 +0200

    edit .py's

--- flask/app/forms.py
+++ flask/app/forms.py
@@ -1,9 +1,38 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+from app import db
+from flask import flash
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('please use a different username.')
+
+def validate_email(self, email):
+    user = User.query.filter_by(email=email.data).first()
+    """cur = get_db().cursor()
+    cur.execute('select email from AllowedEmails')
+    emails = cur.fetchall()
+    raise ValidationError(emails)
+    for i in emails:
+        if i == user:
+            return True
+    else:
+        return False"""
+    if user is not None:
+        raise ValidationError('please use a diffenent email address.')
--- flask/app/models.py
+++ flask/app/models.py
@@ -1,7 +1,11 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
     email = db.Column(db.String(128))
@@ -11,7 +15,16 @@ class User(db.Model):
     def __Repr__(self):
         return '<user {}>'.format(self.username)
 
-def adduser(username, email, admin_access):
-    u = User(username=username, email=email, admin_access=admin_access)
-    db.session.add(u)
-    db.session.commit()
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
