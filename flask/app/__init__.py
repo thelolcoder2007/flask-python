badf2a338f9a7946149b9120f68e90e508ebeeca
thelolcoder2007 <thomas.erents@gmail.com>
2021-07-08 16:13:49 +0200

    Log to both file.log and email (not working yet...)

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -22,9 +23,17 @@ if not app.debug:
         secure = ()
     mail_handler = SMTPHandler(
         mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr = 'no-reply@'+app.config['MAIL_SERVER'],
+        fromaddr = 'thomas.erents@gmail.com',
         toaddrs=app.config["ADMINS"], subject='flask failure!',
         credentials=auth, secure=secure)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/file.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s [in %(pathname)s:%(lineno)id]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('startup, everything up and running!')
 from app import routes, models, errors
