f942ac6516d0de1ea7b7fcbfd14afcd269d2f156
thelolcoder2007 <thomas.erents@gmail.com>
2021-07-12 13:07:59 +0200

    use ssl ipv tls, en gebruik admins voor /admin ipv current_user.admin

--- flask-application/config.py
+++ flask-application/config.py
@@ -6,10 +6,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'user.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER=os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
-    MAIL_PORT=int(os.environ.get('MAIL_PORT')) or 465
-    MAIL_USE_TLS = is.environ.get('MAIL_USE_TLS') is not None
+    MAIL_PORT=int(os.environ.get('MAIL_PORT') or 465)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'simon.vanharingen@gmail.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['thomas.erents@gmail.com']
-    
+    ADMINS = ['thomas']
